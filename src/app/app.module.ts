56d1d48031fbc229d305988e79c8cf45de32e4a3
h324210 <sathish.raj@honeywell.com>
2020-05-17 20:11:35 +0530

    MY MESSAGE HERE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CreditCardFormComponent } from './credit-card-form/credit-card-form.component';
+import { CreditInputComponent } from './credit-input/credit-input.component';
+import { CardDisplayComponent } from './card-display/card-display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreditCardFormComponent,
+    CreditInputComponent,
+    CardDisplayComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
